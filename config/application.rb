c07254e443b53846a0916722c76fc0dc5f7640aa
Carlos <carlos.delgado@hotmail.it>
2020-03-10 14:22:25 +0100

    Turning off Generators in application.rb; fixed typos; erased master key, regenerated a new one and added it in git ignore

--- config/application.rb
+++ config/application.rb
@@ -16,9 +16,7 @@ Bundler.require(*Rails.groups)
 module CocktailApi
   class Application < Rails::Application
     config.load_defaults 6.0
-
     config.api_only = true
-
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
@@ -29,6 +27,14 @@ module CocktailApi
           max_age: 0
       end
     end
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+      generate.helper_specs false
+      generate.routing_specs false
+      generate.controller_specs false
+      config.generators.system_tests = nil
+    end
   end
 end
-
