79e8dc2f0aa149a5db7b0ff257fbc42275033404
Craft Academy <craftacademy@Crafts-MacBook-Air.local>
2020-03-10 19:57:03 +0100

    adds coveralls and rake file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,13 @@
-require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
-
-require File.expand_path('../config/environment', __FILE__)
-
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'coveralls'
 require 'rspec/rails'
 require 'spec_helper'
+Coveralls.wear_merged!('rails')
+
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environments', __FILE__)
+
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+
 
 ActiveRecord::Migration.maintain_test_schema!
 
