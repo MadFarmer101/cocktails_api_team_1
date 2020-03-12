c12a229b5bdbf41ec65eb2243317b02d2f682593
Carlos <carlos.delgado@hotmail.it>
2020-03-12 17:01:47 +0100

    formatting code for PR as request from Kayla

--- app/controllers/api/v1/cocktails_controller.rb
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -1,39 +1,39 @@
 class Api::V1::CocktailsController < ApplicationController
-    before_action :check_query_param
+  before_action :check_query_param
 
-    def index
-        response = RestClient.get(
-            "https://www.thecocktaildb.com/api/json/v1/1/search.php",
-            {
-                params: {
-                    s: params[:q]
-                }
-            }   
-        )
-        
+  def index
+      response = RestClient.get(
+          "https://www.thecocktaildb.com/api/json/v1/1/search.php",
+          {
+              params: {
+                  s: params[:q]
+              }
+          }   
+      )
+      
 
-        if response.body.empty?
-            
-            render json: {error: 'No drinks were found'}, status: 400
-        else
+      if response.body.empty?
           
-            drinks = JSON.parse(response)
-            sanitized_drinks = 
-                {
-                    "name": drinks["drinks"][0]["strDrink"],
-                    "id": drinks["drinks"][0]["idDrink"]
-                }
-            
-            render json: {drinks: sanitized_drinks }
-        end
-    end
+          render json: {error: 'No drinks were found'}, status: 400
+      else
+          
+          drinks = JSON.parse(response)
+          sanitized_drinks = 
+              {
+                  "name": drinks["drinks"][0]["strDrink"],
+                  "id": drinks["drinks"][0]["idDrink"]
+              }
+          
+          render json: {drinks: sanitized_drinks }
+      end
+  end
 
-    private
+  private
 
-    def check_query_param
-        if params[:q] == ""
-            render json: {error: 'No input in search field'}, status: 400
-            return
-        end
-    end
+  def check_query_param
+      if params[:q] == ""
+          render json: {error: 'No input in search field'}, status: 400
+          return
+      end
+  end
 end
