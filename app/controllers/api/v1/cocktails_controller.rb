72a49e708fde08b346fc06db8c227cda18f94333
Craft Academy <craftacademy@Crafts-MacBook-Air.local>
2020-03-13 11:27:08 +0100

    adds funcitonal code to cocktailss_controller

--- app/controllers/api/v1/cocktails_controller.rb
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::CocktailsController < ApplicationController
   before_action :check_query_param
-
   def index
       response = RestClient.get(
           "https://www.thecocktaildb.com/api/json/v1/1/search.php",
@@ -8,27 +7,22 @@ class Api::V1::CocktailsController < ApplicationController
               params: {
                   s: params[:q]
               }
-          }   
+          }
       )
-      
-
       if response.body.empty?
-          
-            drinks = JSON.parse(response)
-           
-            sanitized_drinks = drinks["drinks"].map { |drink| 
-                { 
-                    "id": drink["idDrink"], 
-                    "name": drink["strDrink"]
-                }
-            }
-            
-            render json: {drinks: sanitized_drinks }
-        end
-    end
-
+          render json: {error: 'No drinks were found'}, status: 400
+      else
+          drinks = JSON.parse(response)
+          sanitized_drinks = drinks["drinks"].map { |drink|
+              {
+                  "id": drink["idDrink"],
+                  "name": drink["strDrink"]
+              }
+          }
+          render json: {drinks: sanitized_drinks }
+      end
+  end
   private
-
   def check_query_param
       if params[:q] == ""
           render json: {error: 'No input in search field'}, status: 400
