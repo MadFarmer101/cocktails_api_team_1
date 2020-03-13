0dabd74298660aaa384b178e26d6a15bd30bc055
Janko <mailodjanka@gmail.com>
2020-03-13 17:46:50 +0100

    Creating a mock json file and stubbing a single cocktail response in rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,8 +8,8 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'webmock/rspec'
-#WebMock.enable!
-WebMock.allow_net_connect!
+WebMock.enable!
+#WebMock.allow_net_connect!
 
 ActiveRecord::Migration.maintain_test_schema!
 
@@ -41,5 +41,15 @@ RSpec.configure do |config|
         'Host'=>'www.thecocktaildb.com'
         }).
       to_return(status: 200, body: nil, headers: {})
+
+    stub_request(:get, "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=16158").
+        with(
+          headers: {
+       	  'Accept'=>'*/*',
+       	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+       	  'Host'=>'www.thecocktaildb.com'
+          }).
+        to_return(status: 200, body: file_fixture('cocktails_api_single_cocktail_response.json').read, headers: {})
+
   end
 end
--- spec/requests/api/v1/user_can_see_specific_drink_spec.rb
+++ spec/requests/api/v1/user_can_see_specific_drink_spec.rb
@@ -9,12 +9,7 @@ describe 'GET /cocktails/:id', type: :request do
     end
 
     it 'returns correct cocktail name' do
-      binding.pry
       expect(JSON.parse(response.body)['drink'].first['strDrink']).to eq 'Whitecap Margarita'    
     end
-
-    #it 'returns 200 status' do
-   #   expect(response.status).to eq 200    
-   # end
   end
 end
