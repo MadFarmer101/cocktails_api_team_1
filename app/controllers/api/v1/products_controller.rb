d3f641791932f25c5a49c0405aac7b815e65d438
Carlos <carlos.delgado@hotmail.it>
2020-03-15 17:20:29 +0100

    testing to pass the 204 error

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,10 +1,18 @@
 class Api::V1::ProductsController < ApplicationController
    def index
-    binding.pry
-    RestClient.get('https://www.systembolaget.se/api/productsearch/search/sok-dryck/',
+   
+    response = RestClient.get('https://www.systembolaget.se/api/productsearch/search/sok-dryck/',
         params: {'SearchQuery': 'Vodka'}, 
-        headers: {'Ocp-Apim-Subscription-Key': 'ac91b542647e461daa3620efb4b544ed'})
-
-
+        headers: {'Ocp-Apim-Subscription-Key': '49416a6bf3604134b9a119f4781e5d5d
+            '})
+        binding.pry
+        if response.body.empty?
+            render json: {error: 'No drinks were found'}, status: 400
+          else
+            results = JSON.parse(response.body)
+           
+        
+         end
    end
 end
+
