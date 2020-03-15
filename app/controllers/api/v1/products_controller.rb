52c89135ec1e3e1bbecad400b5b0c6acde877ac8
Janko <mailodjanka@gmail.com>
2020-03-15 18:52:49 +0100

    Writes a new spec to see if the name of the drink is correct

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,14 +1,18 @@
 class Api::V1::ProductsController < ApplicationController
    def index
-    RestClient.get('https://www.systembolaget.se/api/productsearch/search/sok-dryck/',
-    	{
+		response = RestClient.get(
+      "https://www.systembolaget.se/api/productsearch/search/sok-dryck",
+      {
         params: {
-					SearchQuery: params[:q]
-				} 
-        # headers: {
-				# 	"Ocp-Apim-Subscription-Key": "ac91b542647e461daa3620efb4b544ed"
-				# }
-	 		}
-		)
+          type: params[:q]
+        },
+        headers: {
+          'Ocp-Apim-Subscription-Key': 'ac91b542647e461daa3620efb4b544ed'
+        }
+      }
+    )		
+		results = JSON.parse(response)
+		
+		render json: { results: results } 
    end
 end
