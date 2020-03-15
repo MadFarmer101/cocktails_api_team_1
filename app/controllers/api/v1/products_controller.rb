f52d0a38199da5d0dc4156823a6123614f9cd694
Janko <mailodjanka@gmail.com>
2020-03-15 14:37:28 +0100

    Refactoring the products controller

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::ProductsController < ApplicationController
    def index
-    binding.pry
     RestClient.get('https://www.systembolaget.se/api/productsearch/search/sok-dryck/',
-        params: {'SearchQuery': 'Vodka'}, 
-        headers: {'Ocp-Apim-Subscription-Key': 'ac91b542647e461daa3620efb4b544ed'})
-
-
+    	{
+        params: {
+					SearchQuery: params[:q]
+				} 
+        # headers: {
+				# 	"Ocp-Apim-Subscription-Key": "ac91b542647e461daa3620efb4b544ed"
+				# }
+	 		}
+		)
    end
 end
