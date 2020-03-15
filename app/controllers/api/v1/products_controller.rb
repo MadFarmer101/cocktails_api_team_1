eda91c736fdfecdb871a379c439aa772457e1e08
Carlos <carlos.delgado@hotmail.it>
2020-03-15 20:22:19 +0100

    sanitizing the products details for the front end

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -14,8 +14,22 @@ class Api::V1::ProductsController < ApplicationController
     results = JSON.parse(response)
  
     products = results["ProductSearchResults"].first
-		
-    render json: { results: products } 
+
+    sanitized_products = 
+    {
+     product_image: products["Thumbnail"]["ImageUrl"],
+     product_id: products["ProductId"],
+     product_number: products["ProductNumber"],
+     product_name: products["ProductNameBold"],
+     product_name_2: products["ProductNameThin"],
+     product_category: products["Category"],
+     product_price: products["Price"],
+     product_volume: products["Volume"],
+     product_country: products["Country"],
+     product_producer: products["ProducerName"]
+    }
+	
+    render json: { results: sanitized_products } 
    end
 end
 
--- spec/requests/api/v1/user_can_search_products_from_SB_spec.rb
+++ spec/requests/api/v1/user_can_search_products_from_SB_spec.rb
@@ -10,9 +10,52 @@ RSpec.describe Api::V1::ProductsController, type: :request do
     it "should return a 200 response" do
       expect(response).to have_http_status 200
     end
+
     it "should return a correct name of first product" do
-      binding.pry
-      expect(JSON.parse(response.body)["results"]["ProductSearchResults"][0]["ProductNameBold"]).to eq '1850'
+      expect(JSON.parse(response.body)["results"]["product_name"]).to eq '1850'
+    end
+
+    it "should return  product's ID" do
+      expect(JSON.parse(response.body)["results"]["product_id"]).to eq '799020'
+    end
+
+    it "should return product's number" do
+      expect(JSON.parse(response.body)["results"]["product_number"]).to eq '8708201'
+    end
+
+    it "should return product's Price" do
+      expect(JSON.parse(response.body)["results"]["product_price"]).to eq 362.0
+    end
+
+    it "should return product's Country" do
+      expect(JSON.parse(response.body)["results"]["product_country"]).to eq 'Sverige'
+    end
+
+    it "should return product's Volume" do
+      expect(JSON.parse(response.body)["results"]["product_volume"]).to eq 700.0
+    end
+
+    it "should return product's name_2" do
+      expect(JSON.parse(response.body)["results"]["product_name_2"]).to eq 'Vodka'
+    end
+
+    it "should return product's Category" do
+      expect(JSON.parse(response.body)["results"]["product_category"]).to eq 'Sprit'
+    end
+
+
+    it "should return product's ProducerName" do
+      expect(JSON.parse(response.body)["results"]["product_producer"]).to eq 'Tevsjö Destilleri Järvsö AB'
+    end
+
+
+    it "should return product's ProductNumber" do
+      expect(JSON.parse(response.body)["results"]["product_number"]).to eq '8708201'
+    end
+
+
+    it "should return product's Image" do
+      expect(JSON.parse(response.body)["results"]["product_image"]).to eq 'https://static.systembolaget.se/imagelibrary/publishedmedia/dapdo8jq0ivgyr9pkkhl/799020.jpg'
     end
   end
 end
